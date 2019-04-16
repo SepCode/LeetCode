28f3219d6a982b5885e63a512654610ebb44beef
SepCode <usafcheng@gmail.com>
2019-04-16 15:31:57 +0800

    merge

--- LeetCode/LeetCode/ViewController.m
+++ LeetCode/LeetCode/ViewController.m
@@ -8,28 +8,21 @@
 
 #import "ViewController.h"
 #import <objc/runtime.h>
-<<<<<<< HEAD
 #import "Person.h"
 
 typedef struct {
     int a, b, c;
 } TestStruct;
-=======
-//#import <objc/runLoop.h>
->>>>>>> abe05096e4119cf123c2fdcb573e8b2661c68f8f
 
 @interface ViewController (){
     NSObject *_name;
 }
-<<<<<<< HEAD
 
 @property (nonatomic, strong) Person *obj;
-=======
 @property (nonatomic, strong) dispatch_semaphore_t semaphore;
-@property (nonatomic) CFRunLoopActivity obj;
+@property (nonatomic) CFRunLoopActivity activity;
 @property (nonatomic) NSUInteger timeCount;
 @property(nullable, nonatomic,copy) NSArray *title;
->>>>>>> abe05096e4119cf123c2fdcb573e8b2661c68f8f
 @end
 
 
@@ -39,7 +32,6 @@ typedef struct {
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-<<<<<<< HEAD
     
     self.obj = [Person new];
     self.obj.firstName = @"sep";
@@ -60,27 +52,25 @@ typedef struct {
 //    NSValue *testValue = [NSValue value:&test withObjCType:@encode(TestStruct)];
 //    NSArray *array = @[@(1),@"123",testValue];
 //    [self performSelector:@selector(testMoreObj:) withObject:array];
-}
 
-//- (void)testMoreObj:(NSArray *)array {
-//    NSValue *value = array[2];
-//    
-//    TestStruct test;
-//    NSUInteger size = sizeof(TestStruct);
-//    [value getValue:&test size:size];
-//    NSLog(@"%d", test.a);
-//    
-//}
-=======
+
     NSLog(@"%@",NSStringFromClass(self.superclass));
     self.timeCount = 10.0;
     [self registerObserver];
 }
+//- (void)testMoreObj:(NSArray *)array {
+//    NSValue *value = array[2];
+//
+//    TestStruct test;
+//    NSUInteger size = sizeof(TestStruct);
+//    [value getValue:&test size:size];
+//    NSLog(@"%d", test.a);
+//
+//}
 
 - (NSUInteger)timeCount {
     return _timeCount;
 }
->>>>>>> abe05096e4119cf123c2fdcb573e8b2661c68f8f
 
 - (void)setTimeCount:(NSUInteger)timeCount {
     _timeCount = timeCount;
@@ -93,7 +83,7 @@ typedef struct {
 {
 //    CFRunLoopObserverContext context = {0,(__bridge void*)self,NULL,NULL};
     CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
-        self.obj = activity;
+        self.activity = activity;
         dispatch_semaphore_signal(self.semaphore);
     });
     CFRunLoopAddObserver(CFRunLoopGetMain(), observer, kCFRunLoopCommonModes);
@@ -109,7 +99,7 @@ typedef struct {
             long st = dispatch_semaphore_wait(self->_semaphore, dispatch_time(DISPATCH_TIME_NOW, 50*NSEC_PER_MSEC));
             if (st != 0)
             {
-                if (self.obj==kCFRunLoopBeforeSources || self.obj==kCFRunLoopAfterWaiting)
+                if (self.activity==kCFRunLoopBeforeSources || self.activity==kCFRunLoopAfterWaiting)
                 {
                     if (++self->_timeCount < 5)
                         continue;
@@ -119,6 +109,10 @@ typedef struct {
             self->_timeCount = 0;
         }
     });
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    NSLog(@"--");
 }
 
 @end
