c9aca1f4a001118996323db219bdace638d3b575
SepCode <usafcheng@gmail.com>
2019-05-12 17:24:58 +0800

    tcp

--- LeetCode/LeetCode/main.m
+++ LeetCode/LeetCode/main.m
@@ -6,9 +6,9 @@
 //  Copyright © 2019 SepCode. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+//#import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-//#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>
 
 
 
@@ -22,44 +22,49 @@ int main(int argc, char * argv[]) {
 //    __strong id d = c;
 //    NSLog(@"%@",d);
 //    NSLog(@"%@",c);
+    
+    __autoreleasing NSObject *obj = [[NSObject alloc] init];
+    NSLog(@"%@", obj);
+    NSObject *obj2 = obj;
+    NSLog(@"--%@",obj2);
 
-    @autoreleasepool {
-//        CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
-//
-//            /*
-//             kCFRunLoopEntry = (1UL << 0),          进入工作
-//             kCFRunLoopBeforeTimers = (1UL << 1),   即将处理Timers事件
-//             kCFRunLoopBeforeSources = (1UL << 2),  即将处理Source事件
-//             kCFRunLoopBeforeWaiting = (1UL << 5),  即将休眠
-//             kCFRunLoopAfterWaiting = (1UL << 6),   被唤醒
-//             kCFRunLoopExit = (1UL << 7),           退出RunLoop
-//             kCFRunLoopAllActivities = 0x0FFFFFFFU  监听所有事件
-//             */
-//            switch (activity) {
-//                case kCFRunLoopEntry:
-//                    NSLog(@"进入");
-//                    break;
-//                case kCFRunLoopBeforeTimers:
-//                    NSLog(@"即将处理Timer事件");
-//                    break;
-//                case kCFRunLoopBeforeSources:
-//                    NSLog(@"即将处理Source事件");
-//                    break;
-//                case kCFRunLoopBeforeWaiting:
-//                    NSLog(@"即将休眠");
-//                    break;
-//                case kCFRunLoopAfterWaiting:
-//                    NSLog(@"被唤醒");
-//                    break;
-//                case kCFRunLoopExit:
-//                    NSLog(@"退出RunLoop");
-//                    break;
-//                default:
-//                    break;
-//            }
-//        });
-//
-//        CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
-         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+//    @autoreleasepool {
+////        CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+////
+////            /*
+////             kCFRunLoopEntry = (1UL << 0),          进入工作
+////             kCFRunLoopBeforeTimers = (1UL << 1),   即将处理Timers事件
+////             kCFRunLoopBeforeSources = (1UL << 2),  即将处理Source事件
+////             kCFRunLoopBeforeWaiting = (1UL << 5),  即将休眠
+////             kCFRunLoopAfterWaiting = (1UL << 6),   被唤醒
+////             kCFRunLoopExit = (1UL << 7),           退出RunLoop
+////             kCFRunLoopAllActivities = 0x0FFFFFFFU  监听所有事件
+////             */
+////            switch (activity) {
+////                case kCFRunLoopEntry:
+////                    NSLog(@"进入");
+////                    break;
+////                case kCFRunLoopBeforeTimers:
+////                    NSLog(@"即将处理Timer事件");
+////                    break;
+////                case kCFRunLoopBeforeSources:
+////                    NSLog(@"即将处理Source事件");
+////                    break;
+////                case kCFRunLoopBeforeWaiting:
+////                    NSLog(@"即将休眠");
+////                    break;
+////                case kCFRunLoopAfterWaiting:
+////                    NSLog(@"被唤醒");
+////                    break;
+////                case kCFRunLoopExit:
+////                    NSLog(@"退出RunLoop");
+////                    break;
+////                default:
+////                    break;
+////            }
+////        });
+////
+////        CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
+//         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    }
 }
