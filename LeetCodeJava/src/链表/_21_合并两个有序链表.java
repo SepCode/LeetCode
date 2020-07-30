12019aacbcaea0a381e03860f3ed2650ac9acd2b
fei cheng <usafcheng@gmail.com>
2020-07-30 19:53:37 +0800

    链表

---
+++ LeetCodeJava/src/链表/_21_合并两个有序链表.java
@@ -0,0 +1,68 @@
+package 链表;
+
+public class _21_合并两个有序链表 {
+	
+	
+	
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        
+        while (l1 != null && l2 != null) {			
+			if (l1.val <= l2.val) {
+				tail.next = l1;
+				l1 = l1.next;
+			} else {
+				tail.next = l2;
+				l2 = l2.next;
+			}
+			tail = tail.next;
+		}
+        tail.next = (l1 != null) ? l1 : ((l2 != null) ? l2 : null); 
+        
+        return dummy.next;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// 被数组搞混了
+//	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        ListNode dummy = new ListNode(0);
+//        ListNode tail = dummy;
+//        
+//        while (l1 != null || l2 != null) {
+//			if (l1 == null) {
+//				tail.next = l2;
+//				tail = tail.next;
+//				l2 = l2.next;
+//				continue;
+//			}
+//			
+//			if (l2 == null) {
+//				tail.next = l1;
+//				tail = tail.next;
+//				l1 = l1.next;
+//				continue;
+//			}
+//			
+//			if (l1.val <= l2.val) {
+//				tail.next = l1;
+//				l1 = l1.next;
+//			} else {
+//				tail.next = l2;
+//				l2 = l2.next;
+//			}
+//
+//			tail = tail.next;
+//		}
+//        tail.next = null;
+//        
+//        return dummy.next;
+//    }
+}
---
+++ LeetCodeJava/src/链表/_23_合并K个排序链表.java
@@ -0,0 +1,7 @@
+package 链表;
+
+public class _23_合并K个排序链表 {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+}
