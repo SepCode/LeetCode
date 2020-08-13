cea75edfb3933ff7f2e663b49f62de3b525be5e5
fei cheng <usafcheng@gmail.com>
2020-08-13 20:40:43 +0800

    动态规划

---
+++ LeetCodeJava/src/动态规划/_121_买卖股票的最佳时机.java
@@ -0,0 +1,26 @@
+package 动态规划;
+
+public class _121_买卖股票的最佳时机 {
+    public int maxProfit(int[] prices) {
+
+//        输入: [7,1,5,3,6,4]
+//        输出: 5
+//        解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+//        注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+        // 转换为最大子串和
+        // dp[i] = profit[i] + dp[i - 1]
+
+        if (prices == null || prices.length <= 1) return 0;
+        int[] nums = new int[prices.length - 1];
+        int sum = 0, dp = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            nums[i] = prices[i + 1] - prices[i];
+
+            dp = dp > 0 ? dp + nums[i] : nums[i];
+            sum = Math.max(sum, dp);
+        }
+
+        return sum;
+    }
+}
---
+++ LeetCodeJava/src/字符串/_1048_最长字符串链.java
@@ -0,0 +1,39 @@
+package 字符串;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class _1048_最长字符串链 {
+    public int longestStrChain(String[] words) {
+
+        //输入：["a","b","ba","bca","bda","bdca"]
+        //输出：4
+        //解释：最长单词链之一为 "a","ba","bda","bdca"
+
+
+        // 思路：先按照长度排序，存入map，dp[i]是以单词i结尾的最长长度，以单词为key存入dp，
+        //      遍历以i结尾的单词，删除其中一个字符，在map中查找dp最大值;
+
+        // 思路2：遍历单词列表，判断a是否是b的前身，在map中查找a的dp;
+
+
+        Arrays.sort(words, (a, b)->{ return  a.length() - b.length();});
+
+        HashMap<String, Integer> dp = new HashMap();
+        int max = 0;
+        for (String w: words) {
+            int subMax = 0;
+            for (int i = 0; i < w.length(); i++) {
+                String str = w.substring(0, i) + w.substring(i + 1);
+                subMax = Math.max(subMax, dp.getOrDefault(str, 0) + 1);
+            }
+            dp.put(w, subMax);
+            max = Math.max(max, subMax);
+        }
+
+
+
+        return max;
+    }
+}
